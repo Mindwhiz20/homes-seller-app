7c767206e7591e920c62b95f8ee5076f21143d88
saadtanvirMW <saadtanvir@mindwhiz.co>
2021-09-20 18:11:36 +0500

    initial

---
+++ README.md
@@ -0,0 +1,20 @@
+# Real Estate App UI
+
+## [Watch it on YouTube](https://www.youtube.com/watch?v=E8J668JeZAE)
+
+
+Real Estate app UI using flutter,This project consist of Two Screens.
+
+### :heart: Found this project useful?
+
+If you found this project useful, then please consider giving it a :star: on Github and follow me on GitHub.
+
+<a href="https://www.buymeacoffee.com/iamsayuj"><img src="https://cdn.buymeacoffee.com/buttons/v2/default-yellow.png" height="60"></a>
+
+### Real Estate App UI
+
+![App UI](/realestate.gif)
+
+![App UI](/realestateallscr.png)
+
+![App UI](/realestatethumb.png)
---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:real_estate_app/constants/constants.dart';
+import 'package:real_estate_app/screens/home/home_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Real Estate App',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+        scaffoldBackgroundColor: white
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
+
